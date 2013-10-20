1f3682ab1f92779bc9fb1a985bdf4585bf730602
achesak <achesak@yahoo.com>
2013-10-20 18:44:01 -0500

    First commit.

---
+++ gravatar.nim
@@ -0,0 +1,11 @@
+# Gravatar API wrapper.
+
+# Written by Adam Chesak.
+# Code released under the MIT open source license.
+
+# Import modules.
+import httpclient
+import strutils
+import xmlparser
+import xmltree
+import streams
