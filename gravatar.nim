827f09de63de343b89f761a15b060a8719bde65f
achesak <achesak@yahoo.com>
2013-10-20 18:57:50 -0500

    Added base proc definitions.

--- gravatar.nim
+++ gravatar.nim
@@ -9,3 +9,15 @@ import strutils
 import xmlparser
 import xmltree
 import streams
+
+
+# Define the type.
+#type TGravatar*
+
+
+proc getImage*(hash : string): string = 
+    ## Gets the image specified by hash.
+
+
+proc getProfile*(user : string): string = #: TGravatar = 
+    ## Gets the specified user's profile.
