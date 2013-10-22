6f47f882dc5e24cf0bd498ba6665db73e0b15803
achesak <achesak@yahoo.com>
2013-10-22 16:37:06 -0500

    Finished getGravatarImage() and downloadGravatarImage().

--- gravatar.nim
+++ gravatar.nim
@@ -9,15 +9,37 @@ import strutils
 import xmlparser
 import xmltree
 import streams
+import md5
 
 
 # Define the type.
-#type TGravatar*
+#type TGravatarProfile*
 
 
-proc getImage*(hash : string): string = 
-    ## Gets the image specified by hash.
+proc getGravatarHash*(email : string): string = 
+    ## Gets the gravatar hash for email.
+    
+    return getMD5(email)
 
 
-proc getProfile*(user : string): string = #: TGravatar = 
+proc getGravatarImage*(hash : string): string = 
+    ## Gets the image URL specified by hash.
+    
+    return "http://www.gravatar.com/avatar/" & hash
+
+
+proc downloadGravatarImage*(hash : string, filename : string): string = 
+    ## Downloads the image specified by hash. Returns the remote URL.
+    
+    # Create the URL.
+    var url : string = "http://www.gravatar.com/avatar/" & hash
+
+    # Download the image.
+    downloadFile(url, filename)
+    
+    return filename
+    
+
+proc getGravatarProfile*(user : string): string = #: TGravatarProfile = 
     ## Gets the specified user's profile.
+
