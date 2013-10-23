6d86a9e88ec00f04273544309d8164fd52a943fe
achesak <achesak@yahoo.com>
2013-10-23 16:19:03 -0500

    Finished code.

--- gravatar.nim
+++ gravatar.nim
@@ -12,8 +12,14 @@ import streams
 import md5
 
 
-# Define the type.
-#type TGravatarProfile*
+# Define the types.
+type TGravatarPhoto* = tuple[value : string, photoType : string]
+
+type TGravatarProfile* = tuple[id : string, hash : string, requestHash : string, profileUrl : string,
+                               preferredUsername : string, thumbnailUrl : string, photos : seq[TGravatarPhoto],
+                               profileColor : string, profilePosition : string, profileRepeat : string, 
+                               profileBackground : string, givenName : string, familyName : string,
+                               formattedName : string, displayName : string, aboutMe : string, location : string]
 
 
 proc getGravatarHash*(email : string): string = 
@@ -40,8 +46,61 @@ proc downloadGravatarImage*(hash : string, filename : string): string =
     return url
     
 
-proc getGravatarProfile*(user : string): string = #: TGravatarProfile = 
+proc getGravatarProfile*(user : string): TGravatarProfile = 
     ## Gets the specified user's profile.
+    
+    # Get the data.
+    var response : string = getContent("http://www.gravatar.com/" & user & ".xml")
+    
+    # Parse the data into XML.
+    var xml : PXmlNode = parseXML(newStringStream(response)).child("entry")
+    
+    # Create the return object.
+    var profile : TGravatarProfile
+    
+    # Populate the return object.
+    profile.id = xml.child("id").innerText
+    profile.hash = xml.child("hash").innerText
+    profile.requestHash = xml.child("requestHash").innerText
+    profile.profileUrl = xml.child("profileUrl").innerText
+    profile.preferredUsername = xml.child("preferredUsername").innerText
+    if xml.child("thumbnailUrl") != nil:
+        profile.thumbnailUrl = xml.child("thumbnailUrl").innerText
+    if xml.child("photos") != nil:
+        var photosXML : seq[PXmlNode] = xml.findall("photos")
+        var photos = newSeq[TGravatarPhoto](len(photosXML))
+        for i in 0..high(photosXML):
+            var item : TGravatarPhoto
+            item.value = photosXML[i].child("value").innerText
+            if photosXML[i].child("type") != nil:
+                item.photoType = photosXML[i].child("type").innerText
+            photos[i] = item
+        profile.photos = photos
+    var bg : PXmlNode = xml.child("profileBackground")
+    if bg.child("color") != nil:
+        profile.profileColor = bg.child("color").innerText
+    if bg.child("position") != nil:
+        profile.profilePosition = bg.child("position").innerText
+    if bg.child("repeat") != nil:
+        profile.profileRepeat = bg.child("repeat").innerText
+    if bg.child("url") != nil:
+        profile.profileBackground = bg.child("url").innerText
+    var name : PXmlNode = xml.child("name")
+    if name.child("givenName") != nil:
+        profile.givenName = name.child("givenName").innerText
+    if name.child("familyName") != nil:
+        profile.familyName = name.child("familyName").innerText
+    if name.child("formatted") != nil:
+        profile.formattedName = name.child("formatted").innerText
+    if xml.child("displayName") != nil:
+        profile.displayName = xml.child("displayName").innerText
+    if xml.child("aboutMe") != nil:
+        profile.aboutMe = xml.child("aboutMe").innerText
+    if xml.child("currentLocation") != nil:
+        profile.location = xml.child("currentLocation").innerText
+    
+    # Return the profile data.
+    return profile
 
 
 proc getGravatarQR*(user : string): string = 
